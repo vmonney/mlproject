4aef186894d7265bd9182258eee92bb208735758
vmonney <45258650+vmonney@users.noreply.github.com>
2023-08-29 18:53:42 +0200

    Data Transformation done

--- src/logger.py
+++ src/logger.py
@@ -2,8 +2,8 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
