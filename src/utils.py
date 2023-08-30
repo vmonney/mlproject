892bd2e7ab1250ebe430cfebf415f6fee324aa5f
vmonney <45258650+vmonney@users.noreply.github.com>
2023-08-30 23:14:14 +0200

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import os
 import sys
-
-import numpy as np
-import pandas as pd
 import pickle
+
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -16,6 +15,20 @@ def save_object(file_path, obj):
 
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+        model_report = {}
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            model_report[model_name] = r2_score(y_test, y_pred)
+
+        return model_report
 
     except Exception as e:
         raise CustomException(e, sys)
