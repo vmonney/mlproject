26470306d8f52e54105cd77836f6d49185812405
vmonney <45258650+vmonney@users.noreply.github.com>
2023-09-01 14:22:09 +0200

    Deployment tweaks

--- app.py
+++ app.py
@@ -98,4 +98,6 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(
+        host="0.0.0.0"
+    )  # , debug=True) If you want to test it locally activate debug
