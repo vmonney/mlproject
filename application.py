7bd97c7c0c21451ff5e1405f166cf998f97f033d
vmonney <45258650+vmonney@users.noreply.github.com>
2023-09-01 17:05:00 +0200

    added application instead of app

--- application.py
+++ application.py
@@ -99,6 +99,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    application.run(
-        host="0.0.0.0", debug=True
-    )  # If you want to test it locally activate debug
+    application.run(host="0.0.0.0", debug=True)
