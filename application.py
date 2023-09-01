6c52a5662f16c374721a62749474920609dc2981
vmonney <45258650+vmonney@users.noreply.github.com>
2023-09-01 16:17:31 +0200

    resolve issue

--- application.py
+++ application.py
@@ -99,6 +99,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0"
-    )  # , debug=True) If you want to test it locally activate debug
+    app.run(host="0.0.0.0", debug=True)  # If you want to test it locally activate debug
